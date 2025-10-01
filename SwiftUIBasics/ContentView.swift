7422761574b9d2acaa89ecfa397b3db1246cba8a
Ali <ali.cse233@gmail.com>
2025-10-01 13:01:30 +0300

    Initial commit
    Added V and H stacks in this commit

---
+++ SwiftUIBasics/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  SwiftUIBasics
+//
+//  Created by Ali Nawaz on 2025-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .padding(12)
+                    .foregroundStyle(.tint)
+                Text("Hello, all!")
+            }
+            HStack {
+                Image(systemName: "globe")
+                    .padding(12)
+                    .foregroundStyle(.red)
+                Text("This is another string")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Ali Nawaz on 2025-10-01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
